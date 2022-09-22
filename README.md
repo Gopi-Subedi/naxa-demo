d54a49d68e6063c91cf358272fe88c70a8472f97
[Gopi-Subedi] <[gopi.subedi09@gmail.com]>
2022-09-22 21:08:24 +0545

    Completed assigned task

--- README.md
+++ README.md
@@ -68,3 +68,17 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### NAXA Demo
+
+This project is an attempt to replicate the https://naxa.com.np/portfolio/keyhighlights wbsite.
+
+### Library used:
+
+-> SASS (SCSS) ===> npm install node-sass
+-> React Router Dom ===> npm install react-router-dom
+-> React Icon ===> npm install react-icons
+-> Redux/Redux-Toolkit ===> npm install @reduxjs/toolkit
+-> React-Redux ===> npm install react-redux
+-> Redux saga ===> npm install redux-saga
+-> React-Leaflet ==> npm install react-leaflet leaflet
--- src/components/contact/mapSection/basicMap.jsx
+++ src/components/contact/mapSection/basicMap.jsx
@@ -1,6 +1,37 @@
 import React from "react";
 import "./basicMap.scss";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./basicMap.scss";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 export default function BasicMap() {
-  return <div>BasicMap</div>;
+  const mapPosition = [27.704, 85.309];
+  return (
+    <div className="map-container container ">
+      <MapContainer
+        className="map"
+        center={[mapPosition[0], mapPosition[1]]}
+        zoom={13}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
